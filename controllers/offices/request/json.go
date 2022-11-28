653ffbf6a8533e7db82fd3f70129baa31c4c5f1a
faishalmakarim <kamalifaishal1@gmail.com>
2022-11-28 09:26:27 +0700

    new branch

---
+++ controllers/offices/request/json.go
@@ -0,0 +1,23 @@
+package request
+
+// import (
+// 	"backend/businesses/offices"
+// )
+
+// type SearchOfficeImpl struct {}
+
+// func (*SearchOfficeImpl) SearchByCity(city string) []offices.Domain {
+// 	var search []offices.Domain
+
+// 	gorm.DB.Find(&search, "city = ?", city)
+
+// 	return search
+// }
+
+// func (*SearchOfficeImpl) SearchByRate(rate string) []offices.Domain {
+// 	var search []offices.Domain
+
+// 	gorm.DB.Find(&search, "rate = ?", rate)
+
+// 	return search
+// }
--- drivers/domain_factory.go
+++ drivers/domain_factory.go
@@ -2,6 +2,7 @@ package drivers
 
 import (
 	userDomain "backend/businesses/users"
+	// officeDomain "backend/businesses/offices"
 
 	userDB "backend/drivers/mysql/users"
 
@@ -11,3 +12,7 @@ import (
 func NewUserRepository(conn *gorm.DB) userDomain.Repository {
 	return userDB.NewMySQLRepository(conn)
 }
+
+// func OfficeRepository(conn *gorm.DB) officeDomain.OfficeRepository {
+
+// }
